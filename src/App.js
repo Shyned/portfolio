f0868a9defa582a09111cc4cdb566d0779d02e4f
Dyshine Hill <eyedolarts@gmail.com>
2022-07-04 08:28:05 -0500

    created dark and light  mode functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+import { GiSunbeams, GiEvilMoon } from "react-icons/gi";
+export const ThemeContext = createContext("null");
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <section>
+        <div>
+          <label>{theme === "light" ? <GiSunbeams /> : <GiEvilMoon />}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+        <div className="App" id={theme}>
+          <p>Hello</p>
+        </div>
+      </section>
+    </ThemeContext.Provider>
   );
 }
 
