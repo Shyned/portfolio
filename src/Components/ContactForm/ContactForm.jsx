934ff65506f9b0865ca6b88ba47dcba5279b78df
Dyshine Hill <eyedolarts@gmail.com>
2022-07-06 22:07:03 -0500

    contact form for user to be able to send emails

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,55 +1,60 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import { useRef } from "react";
-import { Stack } from "@mantine/core";
+import { Stack, Alert } from "@mantine/core";
+import { TiWarningOutline } from "react-icons/ti";
+import emailjs from "@emailjs/browser";
 
 export default function ContactForm() {
+  const [error, setError] = useState("");
+  const [hasError, setHasError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const emailRef = useRef();
   const subjectRef = useRef();
+  const formRef = useRef();
 
   const handlesubmit = (e) => {
     e.preventDefault();
-    const axios = require("axios");
-
-    const options = {
-      method: "POST",
-      url: "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send",
-      headers: {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": process.env.,
-        "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
-      },
-      data: '{"personalizations":[{"to":[{"email":"eyedolarts@gmail.com"}],"subject":"Hello, World!"}],"from":{"email":"issiahmckeil2@gmail.com"},"content":[{"type":"text/plain","value":"Hello, World!"}]}',
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-        document.getElementById("contact-form").reset();
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    setHasError(false);
+    setLoading(false);
+    emailjs
+      .sendForm(
+        "service_4ke70y2",
+        "template_cfevsg4",
+        formRef.current,
+        "5TYby_Rp3QiKksHEJ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          formRef.current.reset();
+        },
+        (error) => {
+          setError(error.text);
+          setHasError(true);
+        }
+      );
   };
+
   return (
     <div>
-      <form onSubmit={handlesubmit} id="contact-form">
-        {" "}
-        <Stack
-          spacing="xl"
-          sx={(theme) => ({
-            backgroundColor:
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[8]
-                : theme.colors.gray[0],
-            height: 300,
-          })}
+      {hasError === true && (
+        <Alert
+          icon={<TiWarningOutline TiWarning size={16} />}
+          title="Bummer!"
+          color="red"
+          radius="lg"
+          variant="filled"
         >
-          <input ref={emailRef} type="text" placeholder="EMAIL ..."></input>
-
-          <input ref={subjectRef} type="text" placeholder="Subject"></input>
-          <button type="submit ">Send</button>
+          {error}
+        </Alert>
+      )}
+      <form ref={formRef} onSubmit={handlesubmit}>
+        <Stack>
+          <label>Name</label>
+          <input type="text" name="sender" placeholder="Name" />
+          <label>Message</label>
+          <input type="text" name="message" placeholder="Message" />
+          <button typeof="submit">Send</button>
         </Stack>
       </form>
     </div>
